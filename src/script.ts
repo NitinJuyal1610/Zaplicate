e06741984585cf1dca96c5942abda34c845dedc8
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-08-18 16:39:21 +0530

    setup

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log('hey nitin');
+import fs from 'fs/promises';
+import path from 'path';
+import chokidar from 'chokidar';
+
+(async () => {
+  try {
+    const sourcePath = path.join(__dirname, '../', 'sourceFiles');
+    const watcher = chokidar.watch(sourcePath, {
+      ignored: /(^|[\/\\])\../, // ignore dotfiles
+      persistent: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
