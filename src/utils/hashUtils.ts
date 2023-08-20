c25bbc7d87f0649c3c402c52c662ec5fdc3bc757
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-08-20 23:07:42 +0530

    hashStream created

--- src/utils/hashUtils.ts
+++ src/utils/hashUtils.ts
@@ -1,8 +1,10 @@
 import crypto from 'crypto';
 import fs from 'fs';
+const { finished } = require('node:stream/promises');
 import { HashingStream } from '../custom/hashingStream';
 
 export const hashFile = async (filePath: string): Promise<string> => {
+  let hash = '';
   try {
     //calculate hash
     const readStream = fs.createReadStream(filePath);
@@ -11,11 +13,17 @@ export const hashFile = async (filePath: string): Promise<string> => {
       encoding: 'hex',
     });
 
-    const hasedStream = readStream.pipe(hashingStream);
-    console.log(hasedStream);
+    const hashedStream = readStream.pipe(hashingStream);
+
+    hashedStream.on('data', (chunk) => {
+      hash = chunk.toString();
+    });
+
+    await finished(hashedStream);
   } catch (err) {
     console.log('Error: ', err);
     throw err;
   }
-  return 'some hash';
+
+  return hash;
 };
